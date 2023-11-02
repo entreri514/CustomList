bf37e5884983301a868e35ae3de1b7a90bced61c
entreri514 <148565529+entreri514@users.noreply.github.com>
2023-11-02 13:08:11 -0500

    Five initial tests started

--- CustomListTests/AddMethodTests.cs
+++ CustomListTests/AddMethodTests.cs
@@ -6,8 +6,63 @@ namespace CustomListTests
     public class AddMethodTests
     {
         [TestMethod]
-        public void TestMethod1()
+
+        public void AddMethod_CountAsItemsAdded_CountIncrementsAsItemsAdded()
         {
+            //Arrange
+
+            //Act
+
+            //Assert
+        }
+
+        [TestMethod]
+
+        public void AddMethod_AddOneItem_FirstItemAtIndexZero()
+        {
+            //Arrange
+            CustomList<string> myList = new CustomList<string>();
+            //Act
+
+            myList.Add("Hello");
+
+            //Assert
+            Assert.AreEqual("Hello", myList[0]);
+        }              
+
+        [TestMethod]
+        public void AddMethod_AddTwoItems_SecondItemAtIndexOne()
+        {
+            //Arrange
+            CustomList<string> myList = new CustomList<string>();
+            //Act
+            myList.Add("Hello");
+            myList.Add("world");
+
+            //Assert
+            Assert.AreEqual("world", myList[1]);
+        }
+
+        [TestMethod]
+
+        public void AddMethod_CapacityIncreaseWhenExceeded_CapacityIncreased()
+        {
+            //Arrange
+
+            //Act
+
+            //Assert
+        }
+
+        [TestMethod]
+
+        public void AddMethod_AfterCapacityIncreases_OriginalItemInOriginalIndex()
+        {
+            //Arrange
+
+            //Act
+
+            //Assert
         }
     }
 }
